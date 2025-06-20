3188082e267c8e8ff70153493127307bf649c162
sami-dot <samiseymen1010@gmail.com>
2025-06-20 16:07:18 +0300

    power

--- obj/Debug/net8.0/SimpleIISApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleIISApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleIISApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5efd16e1fd47fdbb306121e999e5410ac80be400")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6cca134906419b08e56a6b09f7f4cd23cd3a831")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleIISApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleIISApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
